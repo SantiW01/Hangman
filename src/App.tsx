db6bf05c429716ff23a7b2a890adbee166d0700b
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-18 20:19:27 -0300

    Adding Json's words

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 
 function App() {
+  const wordList = require("./WordList.json");
+  const [word, setWord] = useState<String>("");
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
