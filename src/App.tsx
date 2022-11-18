e27eea2e8b958ffc5fc212d3f2d1dc448d086513
Santiago Weihmüller <61631736+SantiW01@users.noreply.github.com>
2022-11-18 20:49:07 -0300

    Adding function for generate random number
    As index of the word chosen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
 
+function generateRandomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 function App() {
-  const wordList = require("./WordList.json");
+  const wordList: string = require("./WordList.json");
   const [word, setWord] = useState<String>("");
+  const wordChosen: number = generateRandomInt(0, 2886);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
